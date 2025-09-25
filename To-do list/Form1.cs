c036f677099f64740dc76165a35f127103a90147
Akylbenko <licenzeeya@gmail.com>
2025-09-25 17:47:45 +1000

    Box adding

--- To-do list/Form1.cs
+++ To-do list/Form1.cs
@@ -12,9 +12,29 @@ namespace To_do_list
 {
     public partial class Form1 : Form
     {
+        private TextBox taskTextBox;
+        private ListBox tasksListBox;
+
         public Form1()
         {
             InitializeComponent();
+            FormSetup();
+        }
+
+        private void FormSetup()
+        {
+            this.Size = new Size(400, 400);
+
+            taskTextBox = new TextBox();
+            taskTextBox.Location = new Point(20, 50);
+            taskTextBox.Size = new Size(340, 20);
+            this.Controls.Add(taskTextBox);
+
+            
+            tasksListBox = new ListBox();
+            tasksListBox.Location = new Point(20, 110);
+            tasksListBox.Size = new Size(340, 250);
+            this.Controls.Add(tasksListBox);
         }
     }
 }
